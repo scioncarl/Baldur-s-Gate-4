58ca530de4c1861726c8e969acd6d2cb28c688ea
Titouan van rompaey <116149382+Titouanvanrompaey@users.noreply.github.com>
2023-09-26 11:57:09 +0200

    Add files via upload

---
+++ kotlinAventureStart-master/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import item.Qualite
+import jeu.Jeu
+import personnage.Personnage
+
+//instanciation des qualités des objets
+val qualiteCommun = Qualite("commun", 0, "\u001B[32m")
+val qualiteRare = Qualite("rare", 1, couleur = "\u001B[34m")
+val qualiteEpic = Qualite("epic", 2, "\u001B[35m")
+val qualiteLegendaire = Qualite("legendaire", 3, "\u001B[33m")
+
+fun main(args: Array<String>) {
+    //Instantiation des monstres
+    val gobelin = Personnage(
+        "XXX le gobelin",
+        pointDeVie = 20,
+        pointDeVieMax = 20,
+        attaque = 5,
+        defense = 4,
+        vitesse = 11,
+        endurance = 6)
+    // TODO Intermission 1 Ajouter d'autres monstres
+    //On ajoute les monstres a la liste de monstres du jeu
+    val jeu = Jeu(listOf( gobelin))
+    //Lancement du jeu
+    jeu.lancerCombat()
+}
